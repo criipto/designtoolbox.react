63538b4535ae02f4095e60627a292eeefbafbf1d
Rune Lund-Søltoft <rune.lund.soltoft@criipto.com>
2022-02-01 11:21:28 +0100

    Working on DX in vanilla JS

--- src/Index.fs
+++ src/Index.fs
@@ -5,7 +5,29 @@ open Feliz.Bulma
 
 [<AutoOpen>]
 module Components = 
-      let Navbar(options) = Navbar.Navbar(options)
+      
+      let Navbar (options : {| 
+            LogOutText : string
+            LogInText : string
+            AppName : string
+            Manager : {| 
+                         ViewManager : IViewManager<'view>
+                         UserManager : IUserManager<'user>
+                         ErrorManager : IErrorManager<'err>
+                      |}
+            |}) = 
+            Navbar.Navbar(
+                  { 
+                      LogOutText = options.LogOutText
+                      LogInText = options.LogInText
+                      AppName = options.AppName
+                      Manager = 
+                          { new IManager<_,_,_> with
+                              member __.ViewManager with get() = options.Manager.ViewManager
+                              member __.UserManager with get() = options.Manager.UserManager
+                              member __.ErrorManager with get() = options.Manager.ErrorManager
+                          }
+                  })
       let SidePanelMenu(options) = SidePanelMenu.SidePanelMenu(options)
       let Table(options) = Table.Table(options)
       let Layout(options) = Layout.Layout(options)
